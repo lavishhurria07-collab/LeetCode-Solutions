3f1e1f1f5d002f1d42da25b2d2adc32ddab20658
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-07-14 17:14:49 +0530

    Time: 4 ms (26.47%), Space: 8.9 MB (65.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    long long rev = 0 ;
+    long long c = x ;
+    while ( x > 0 ) { 
+        rev = rev * 10 + x % 10 ;
+        x = x / 10 ; 
+    }
+    if ( c == rev ) { 
+        return true ;
+    }
+    else {
+        return false ;
+    }
+}
