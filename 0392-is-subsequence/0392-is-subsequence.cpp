278982267812f3e9de57b8e13fe303392179ea73
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-01 23:56:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.17%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -5,10 +5,8 @@ public:
         int pointer1 = 0 ; 
         int m = s.size() ; 
         int pointer2 = 0 ;
-        int count = 0 ; 
         while ( ( pointer1 < n ) && ( pointer2 < m ) ) { 
             if ( s[pointer2] == t[pointer1] ) { 
-                count++ ; 
                 pointer2++ ; 
             } 
             pointer1++ ; 
