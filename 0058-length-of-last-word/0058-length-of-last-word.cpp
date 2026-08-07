7e70b89d74b22935fbc8809c0a79d99d687f9b56
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-07 23:38:34 +0530

    Time: 0 ms (100%), Space: 9 MB (8.92%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end = s.length() - 1 ; 
+        int count = 0 ; 
+        while( ( end >= 0 ) && ( s[end] == ' ' ) ) { 
+            end-- ; 
+        }
+        while ( ( end >= 0 )  && ( s[end] != ' ') ){
+            count++;
+            end--;
+        }
+        return count ; 
+    }
+};
