7a1fd84453b0980aefd0182fe79a63f5aab23e59
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-18 23:26:23 +0530

    Time: 16 ms (59.05%), Space: 26.1 MB (50.92%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size() ; 
+        unordered_map<string,vector<string>> mp;
+        for ( int i = 0 ; i < n ; i++ ) {
+            string str = strs[i] ; 
+            sort(str.begin(),str.end());
+            mp[str].push_back(strs[i]);
+        }
+        vector<vector<string>> ans ; 
+        for ( auto x : mp ) {
+            ans.push_back(x.second);
+        }
+        return ans ;
+    }
+};
