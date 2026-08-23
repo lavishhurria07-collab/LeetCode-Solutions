a2b7f14f010a70296c9ea8dcd028c960b056e2de
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-23 20:13:03 +0530

    Time: 0 ms (100%), Space: 22.4 MB (7.78%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size() ; 
-        sort(nums.begin(),nums.end()) ; 
-        int counter = 0 ; 
-        int i = 0 ;
-        while ( i < n ) { 
-            if ( nums[i] != counter ) { 
-                return counter ; 
-            }
-            i++ ; 
-            counter++ ; 
+        vector<int> frequency(n + 1, 0);
+        for ( int i = 0 ; i < n ; i++ ) { 
+            frequency[nums[i]]++ ; 
         }
-        return counter ; 
+        for ( int i = 0 ; i <= n ; i++ ) { 
+            if ( frequency[i] == 0 ) { 
+                return i ;
+            }
+        }
+        return 0 ; 
+        // int n = nums.size() ; 
+        // int counter = 0 ; 
+        // int i = 0 ;
+        // sort(nums.begin(),nums.end()) ; 
+        // while ( i < n ) { 
+        //     if ( nums[i] != counter ) { 
+        //         return counter ; 
+        //     }
+        //     i++ ; 
+        //     counter++ ; 
+        // }
+        // return counter ; 
     }
 };
