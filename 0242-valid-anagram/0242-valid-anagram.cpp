6e922f80fbc7a9129462465a33951b719fe54a0d
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-26 15:31:05 +0530

    Time: 5 ms (25.35%), Space: 9.7 MB (49.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,20 +5,15 @@ public:
         int m = t.size() ; 
         if ( m != n ) { 
             return false ; 
-        }
-        else { 
-            int frequency[26] = {0} ; 
-            for ( int i = 0 ; i < n ; i++ ) { 
-                frequency[s[i]-'a']++ ; 
+        } 
+        int left = 0 ;  
+        sort(s.begin(),s.end()) ; 
+        sort(t.begin(),t.end()) ; 
+        while ( left < n ) { 
+            if ( s[left] != t[left] ) { 
+                return false ; 
             }
-            for ( int j = 0 ; j < m ; j++ ) { 
-                frequency[t[j]-'a']-- ; 
-            }
-            for ( int k = 0 ; k < 26 ; k++ ) { 
-                if ( frequency[k] != 0 ) { 
-                    return false ; 
-                }
-            }
+            left++ ; 
         }
         return true ; 
     }
