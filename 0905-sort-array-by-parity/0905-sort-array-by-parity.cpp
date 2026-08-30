3945107731ad70090b038b4ea3c3ac0385a3e9f3
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-30 12:09:30 +0530

    Time: 0 ms (100%), Space: 20.8 MB (21.85%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
+    vector<int> sortArrayByParity(vector<int>& nums) { 
+        sort(nums.begin(),nums.end()) ; 
         int n = nums.size() ; 
         int left = 0 ; 
-        int right = n - 1 ;   
-        while ( right > left ) { 
-            while ( nums[left] % 2 != 1 ) { 
-                left++ ; 
-            } // odd number 
-            while ( nums[right] % 2 != 0 ) { 
+        int right = n - 1 ; 
+        while ( left < right ) { 
+            while ( nums[right] % 2 != 0 && left < right ) { 
                 right-- ; 
-            } // even number
+            }
+            while ( nums[left] % 2 != 1 && left < right ) { 
+                left++ ; 
+            }
             swap(nums[left],nums[right]) ; 
             left++ ; 
             right-- ; 
         }
-        return nums; 
+        return nums ; 
     }
 };
