a0a183d919de517436427864da7a7ec3fb72b826
lavishhurria07-collab <lavishhurria07@gmail.com>
2026-08-31 00:41:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (90.91%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,19 +11,27 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *temp = head ; 
-        int count = 0 ; 
-        while ( temp != NULL ) { 
-            temp = temp -> next ; 
-            count++ ; 
-        }
-        if ( count == 1 ) { 
-            return head ; 
-        }
-        temp = head ; 
-        for ( int i = 0 ; i < ( count / 2 - 1 ) ; i++ ) { 
-            temp = temp -> next ; 
-        }
-        return temp -> next ; 
+        // ListNode *temp = head ; 
+        // int count = 0 ; 
+        // while ( temp != NULL ) { 
+        //     temp = temp -> next ; 
+        //     count++ ; 
+        // }
+        // if ( count == 1 ) { 
+        //     return head ; 
+        // }
+        // temp = head ; 
+        // for ( int i = 0 ; i < ( count / 2 - 1 ) ; i++ ) { 
+        //     temp = temp -> next ; 
+        // }
+        // return temp -> next ; 
+        // SLOW AND FAST POINTER APPROACH . 
+        ListNode *fast = head ;
+        ListNode *slow = head ;  
+        while ( fast != NULL && fast -> next != NULL ) { 
+            fast = fast->next->next ; 
+            slow = slow -> next ; 
+        } 
+        return slow ; 
     }
 };
